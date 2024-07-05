6bd3afbc7343cadf8095b48b8e62d6baadeef80f
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2024-07-05 12:21:53 -0400

    initial changes before rebase

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,18 @@
 import { Resolvers } from "../generated/graphql";
+import contributorResolvers from "./contributorResolvers";
+import otherContribItemResolvers from "./otherContribItemResolvers";
 import userResolvers from "./userResolvers.js";
 
 const resolvers: Resolvers = {
-  ...userResolvers,
+  Query: {
+    ...userResolvers.Query,
+    ...contributorResolvers.Query,
+    ...otherContribItemResolvers.Query,
+  },
+  Mutation: {
+    ...userResolvers.Mutation,
+    ...contributorResolvers.Mutation,
+    ...otherContribItemResolvers.Mutation,
+  },
 };
 export default resolvers;
--- src/resolvers/otherContribItemResolvers.ts
+++ src/resolvers/otherContribItemResolvers.ts
@@ -1,24 +1,71 @@
-// import { randomUUID } from "crypto";
-// import { otherContributions } from "../__mock__";
+import prisma from "../../libs/prisma";
+import { logger } from "../utils/Logger";
 
-// export default {
-//   Query: {
-//     otherContribItem(id) {
-//       const mock = otherContributions[0];
+export default {
+  Query: {
+    otherContribItem: async (parent, args, context, info) => {
+      const { id } = args;
 
-//       return {
-//         id: randomUUID(),
-//         contributionDate: mock.date,
-//         contributor: {
-//           annualSalary: 123,
-//           benRatePer: 0.2,
-//           dailyHours: 12,
-//           userId: randomUUID(),
-//         },
-//         details: mock.details,
-//         name: "name",
-//         value: mock.value,
-//       };
-//     },
-//   },
-// };
+      logger.info(`querying other contribution item with id: ${id}`);
+      try {
+        const otherContribItem = await prisma.otherContribItem.findFirst({
+          where: { id },
+        });
+
+        return { ...otherContribItem };
+      } catch (error) {
+        logger.error(`error with other contribution item query: ${error}`);
+        return null;
+      }
+    },
+  },
+  Mutation: {
+    createOtherContribItem: async (parent, args, context, info) => {
+      const {
+        contributorId,
+        ikcReportId,
+        name,
+        contributionDate,
+        value,
+        details,
+      } = args;
+
+      logger.info(
+        `creating other contribution item with contributorId: ${contributorId}, name: ${name}, contributionDate: ${contributionDate}, value: ${value}, details: ${details}`
+      );
+
+      try {
+        const ikcReport = await prisma.iKCReport.findUniqueOrThrow({
+          where: { id: ikcReportId },
+        });
+        const contributor = await prisma.contributor.findUniqueOrThrow({
+          where: { id: contributorId },
+        });
+
+        if (contributor && ikcReport) {
+          const otherContribItem = await prisma.otherContribItem.create({
+            data: {
+              contributorId,
+              ikcReportId,
+              name,
+              contributionDate,
+              value,
+              details,
+            },
+          });
+
+          return otherContribItem?.id;
+        } else {
+          throw new Error(
+            `contributor and/or ikc report not matching in database`
+          );
+        }
+      } catch (error) {
+        logger.error(`failed to create other contribution item: ${error}`);
+      }
+    },
+    removeOtherContribItem: async (parent, args, context, info) => {
+      const { userId, itemId } = args;
+    },
+  },
+};
