85a43541c0cfde12f9d1cc2dce62d126da1f7715
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2024-07-29 16:30:43 -0400

    final fixes

--- src/utils/reducers.ts
+++ src/utils/reducers.ts
@@ -145,6 +145,7 @@ export const getIKCReportArray = (ikcReports): IkcReport[] => {
       {
         id: currentValue.id,
         partnerOrgId: currentValue.partnerOrgId,
+        researchProjectId: currentValue.researchProjectId,
         reportStartDate: currentValue.reportStartDate,
         submissionDate: currentValue.submissionDate,
         contributions: getContributionsArray(currentValue.Contributions),
@@ -158,7 +159,7 @@ export const getIKCReportArray = (ikcReports): IkcReport[] => {
 };
 
 export const getResearchProject = async (id) => {
-  const researchProject = await prisma.researchProject.findUnique({
+  const researchProject = await prisma.researchProject.findUniqueOrThrow({
     where: { id },
     include: {
       admins: true,
@@ -169,6 +170,7 @@ export const getResearchProject = async (id) => {
         select: {
           id: true,
           partnerOrgId: true,
+          researchProjectId: true,
           reportStartDate: true,
           submissionDate: true,
           Contributions: {
